c4108703dcea10beedaa0c9fae88f5a86b89c40f
knightnight1 <knightnight1@github.com>
2017-01-07 10:35:15 -0800

    we made some more changes to tic tac toe

---
+++ users/knightnight1/arrays.js
@@ -0,0 +1,17 @@
+var a = [1, 2, 3]
+console.log(a);
+a.forEach(n  => console.log(n));
+
+var xue = ["farts", "smell", "realy", "bad"];
+console.log(xue);
+xue.forEach(n  => console.log(n));
+
+var b = [["o", "o", "x"],
+         ["x", "x", "o"],
+         ["x", "o", "o" ]];
+console.log(b[0])
+console.log(`${b[0][0]}  | ${b[0][1]} | ${b[0][2]}`);
+console.log(`---+---+---`);
+console.log(`${b[1][0]} | ${b[1][1]} | ${b[1][2]}`);
+console.log(`---+---+---`);
+console.log(`${b[2][0]} | ${b[2][1]} | ${b[2][2]}`);
---
+++ users/knightnight1/asciiArt.js
@@ -0,0 +1,13 @@
+console.log("    / \\       __________________");
+console.log("   /   \\      |  _______________|     o        o       o");
+console.log("  /  o  \\     / /_|");
+console.log(" /       \\   |                                                |");
+console.log("/_________\\  |_|");
+
+console.log("            0")
+console.log("          /||\\")
+console.log("        // /\\")
+console.log("_______//      _______");
+console.log("        |      |");
+console.log("        |      |");
+console.log("         ||||||");
