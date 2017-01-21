08d3d2b092b811e5c1911dc2e54c06034428df10
knightnight1 <knightnight1@github.com>
2017-01-21 10:22:07 -0800

    more comits

--- users/knightnight1/tictactoe.js
+++ users/knightnight1/tictactoe.js
@@ -1,12 +1,16 @@
 const prompt = require('prompt-sync')();
 
-console.log("hello user this is tic tac toe the other person is going to kick your butt");
+console.log("hello user this is tic tac toe the other person is going to kick your butt. type q to exit");
 var board = [[" ", " ", " "],
             [" ", " ", " "],
             [" ", " ", " " ]];
 
 while (true) {
     var row = prompt('Enter row number 1-3: ');
+    if (row === "q"){
+        console.log("quitting");
+        process.exit();
+    }
     var col = prompt('Enter column number 1-3: ');
     console.log(`${row},${col}`);
     drawboard(board);
